6a7e2d615d2f9a1c4c9aa7c67dbd7ae3da50e374
Marvellous Ubani <marvellousubani@gmail.com>
2017-09-28 09:56:50 +0100

    hey

--- mysite/settings.py
+++ mysite/settings.py
@@ -71,6 +71,8 @@ INSTALLED_APPS = [
     'django.contrib.sites',
 ]
 SITE_URL = 'https://www.avetizblog.com'
+SECURE_SSL_REDIRECT = True
+# SSLIFY_DISABLE = True
 
 SITE_ID = 1
 
@@ -84,6 +86,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     #'newsletter_signup.middleware.GetRefererMiddleware',
 ]
+
+MIDDLEWARE_CLASSES = (
+    'sslify.middleware.SSLifyMiddleware',
+)
 
 ROOT_URLCONF = 'mysite.urls'
 #FROM_EMAIL='myschoolrents@gmail.com'
