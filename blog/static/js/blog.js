e088d8de3e249191c2153506439b5da13aa23c05
Marvellous Ubani <marvellousubani@gmail.com>
2017-09-30 18:36:44 +0100

    'h'

--- blog/static/js/blog.js
+++ blog/static/js/blog.js
@@ -212,7 +212,7 @@ var makePayt = function () {
 
 $('#commentForm').on('submit', function(event){
     event.preventDefault();
-    console.log("form submitted!")  // sanity check
+    alert("comment submited")  // sanity check
     create_comment();
 });
 
