c181757591dd5c143fe6e1d72ce5225e98794f93
Marvellous Ubani <marvellousubani@gmail.com>
2017-10-02 08:52:40 +0100

    'h'

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.decorators import login_required
